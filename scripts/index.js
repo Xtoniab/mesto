1b94b18fc703d6eec28f1cba9717fda64e0206ed
Yaroslav Sarchuk <y.sarchuk@eidoloncorp.com>
2022-12-19 05:25:03 +0400

    fix: Rename some blocks & Fix BEM structure

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,38 @@
 let profileNameElement = document.querySelector('.profile__name');
 let profileCaptionElement = document.querySelector('.profile__caption');
+let profileEditBtn = document.querySelector('.profile__edit-btn');
 
-let editProfileBtn = document.querySelector('.profile__edit-btn');
-let editProfilePopup = document.querySelector('.edit-profile-popup');
-let editProfilePopupCloseBtn = document.querySelector('.edit-profile-form__close-btn');
-let editProfilePopupNameInput = document.querySelector('.edit-profile-form__item_type_name');
-let editProfilePopupCaptionInput = document.querySelector('.edit-profile-form__item_type_caption');
+let popup = document.querySelector('.popup');
+let popupCloseBtn = document.querySelector('.popup__close-btn');
+let editProfileFormNameInput = document.querySelector('.edit-profile-form__item_type_name');
+let editProfileFormCaptionInput = document.querySelector('.edit-profile-form__item_type_caption');
 let editProfileForm = document.querySelector('.edit-profile-form');
 
 let likesBtns = document.querySelectorAll('.place__like-btn');
 
-editProfileBtn.addEventListener('click', openEditProfilePopup);
-editProfilePopupCloseBtn.addEventListener('click', closeEditProfilePopup);
+profileEditBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
 editProfileForm.addEventListener('submit', saveProfileChangesAndClosePopup);
 
-likesBtns.forEach(function(btn) {
-  btn.addEventListener('click', function() {
+likesBtns.forEach(function (btn) {
+  btn.addEventListener('click', function () {
     btn.classList.toggle('place__like-btn_active');
   });
 });
 
 function saveProfileChangesAndClosePopup(evt) {
   evt.preventDefault();
-  profileNameElement.textContent = editProfilePopupNameInput.value;
-  profileCaptionElement.textContent = editProfilePopupCaptionInput.value;
-  closeEditProfilePopup();
+  profileNameElement.textContent = editProfileFormNameInput.value;
+  profileCaptionElement.textContent = editProfileFormCaptionInput.value;
+  closePopup();
 }
 
-function openEditProfilePopup() {
-  editProfilePopupNameInput.value = profileNameElement.textContent;
-  editProfilePopupCaptionInput.value = profileCaptionElement.textContent;
-  editProfilePopup.classList.add('edit-profile-popup_opened');
+function openPopup() {
+  editProfileFormNameInput.value = profileNameElement.textContent;
+  editProfileFormCaptionInput.value = profileCaptionElement.textContent;
+  popup.classList.add('popup_opened');
 }
 
-function closeEditProfilePopup() {
-  editProfilePopup.classList.remove('edit-profile-popup_opened');
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
